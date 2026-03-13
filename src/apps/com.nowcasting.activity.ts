377f788d184403eedaa2bfc3f6c17c524db0f36a
JohnHsu <syujen_1997@163.com>
2026-03-13 14:25:44 +0800

    根据实测，修改关于彩云天气的相应规则，待测试

--- src/apps/com.nowcasting.activity.ts
+++ src/apps/com.nowcasting.activity.ts
@@ -37,26 +37,32 @@ export default defineGkdApp({
       fastQuery: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
+        // {
+        //   key: 0,
+        //   matches:
+        //     '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] <n FrameLayout >(2,3) [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+        //   snapshotUrls: [
+        //     'https://i.gkd.li/i/13405131',
+        //     'https://i.gkd.li/i/14814345',
+        //   ],
+        // },
+        // {
+        //   key: 1,
+        //   matches:
+        //     'ImageView[childCount=0] < @ViewGroup[clickable=true][childCount=1] < ViewGroup <5 ViewGroup + ViewGroup [text="广告"]',
+        //   snapshotUrls: 'https://i.gkd.li/i/14814374',
+        // },
+        // {
+        //   key: 2,
+        //   matches: '[vid="entrance_image"] + [vid="close"]',
+        //   exampleUrls: 'https://e.gkd.li/bd53a023-f83a-45ae-b58e-c7369f29bb2e',
+        //   snapshotUrls: 'https://i.gkd.li/i/16965108',
+        // },
         {
-          key: 0,
-          matches:
-            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] <n FrameLayout >(2,3) [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/13405131',
-            'https://i.gkd.li/i/14814345',
-          ],
-        },
-        {
-          key: 1,
-          matches:
-            'ImageView[childCount=0] < @ViewGroup[clickable=true][childCount=1] < ViewGroup <5 ViewGroup + ViewGroup [text="广告"]',
-          snapshotUrls: 'https://i.gkd.li/i/14814374',
-        },
-        {
-          key: 2,
-          matches: '[vid="entrance_image"] + [vid="close"]',
-          exampleUrls: 'https://e.gkd.li/bd53a023-f83a-45ae-b58e-c7369f29bb2e',
-          snapshotUrls: 'https://i.gkd.li/i/16965108',
+          key: 3,
+          // JohnHsu 关闭弹窗广告的关闭按钮
+          matches: 'TextView[vid="close_btn"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25959135',
         },
       ],
     },
@@ -66,16 +72,28 @@ export default defineGkdApp({
       fastQuery: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
+        // {
+        //   key: 1,
+        //   matches: '[id="com.nowcasting.activity:id/ll_close"]',
+        //   snapshotUrls: 'https://i.gkd.li/i/13690822',
+        // },
+        // {
+        //   preKeys: [1],
+        //   key: 2,
+        //   matches: '[id="com.nowcasting.activity:id/ll_uninterested"]',
+        //   snapshotUrls: 'https://i.gkd.li/i/13690830',
+        // },
         {
-          key: 1,
-          matches: '[id="com.nowcasting.activity:id/ll_close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13690822',
+          key: 3,
+          // JohnHsu 关闭 ad_card1 下的广告 ImageView
+          matches: '[id="com.nowcasting.activity:id/ad_card1"] ImageView[text=null][width<48 && height<48][visibleToUser=true] ',
+          snapshotUrls: 'https://i.gkd.li/i/25959233',
         },
         {
-          preKeys: [1],
-          key: 2,
-          matches: '[id="com.nowcasting.activity:id/ll_uninterested"]',
-          snapshotUrls: 'https://i.gkd.li/i/13690830',
+          key: 4,
+          // JohnHsu 关闭 ad_card2 下的广告 ImageView
+          matches: '[id="com.nowcasting.activity:id/ad_card2"] ImageView[text=null][width<48 && height<48][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25959241',
         },
       ],
     },
@@ -86,43 +104,44 @@ export default defineGkdApp({
       fastQuery: true,
       activityIds: 'com.nowcasting.activity.WeatherActivity',
       rules: [
-        {
-          key: 0,
-          matches: '[id="com.nowcasting.activity:id/jad_feed_close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13690826',
-        },
-        {
-          key: 1,
-          matches:
-            '@FrameLayout[visibleToUser=true] +2 * > [text="广告"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/14814348',
-        },
+        // {
+        //   key: 0,
+        //   matches: '[id="com.nowcasting.activity:id/jad_feed_close"]',
+        //   snapshotUrls: 'https://i.gkd.li/i/13690826',
+        // },
+        // {
+        //   key: 1,
+        //   matches:
+        //     '@FrameLayout[visibleToUser=true] +2 * > [text="广告"][visibleToUser=true]',
+        //   snapshotUrls: 'https://i.gkd.li/i/14814348',
+        // },
         {
           key: 2,
-          // JohnHsu 提示手机号快速登陆
+          // JohnHsu 关闭手机号快速登陆小卡片。小心使用，开启后会在想主动登陆时反而GKD关闭登陆界面
           matches:
             'ImageButton[id=null][width>100 && height>100][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/25957726',
         },
       ],
     },
-    {
-      key: 5,
-      name: '功能类-点击收起顶部工具栏',
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.nowcasting.activity.WeatherActivity',
-          matches:
-            'ViewGroup[childCount=2][visibleToUser=true] > [vid="fl_arrow"][visibleToUser=true]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/5bdf4f45-d66d-441e-a2ce-f32d5fc40796',
-          snapshotUrls: [
-            'https://i.gkd.li/i/14814347', // 收起前
-            'https://i.gkd.li/i/14814412', // 收起后
-          ],
-        },
-      ],
-    },
+    // {
+    // 不建议开启，会导致想收起顶部工具栏时GKD误打开
+    //   key: 5,
+    //   name: '功能类-点击收起顶部工具栏',
+    //   rules: [
+    //     {
+    //       fastQuery: true,
+    //       activityIds: 'com.nowcasting.activity.WeatherActivity',
+    //       matches:
+    //         'ViewGroup[childCount=2][visibleToUser=true] > [vid="fl_arrow"][visibleToUser=true]',
+    //       exampleUrls:
+    //         'https://m.gkd.li/57941037/5bdf4f45-d66d-441e-a2ce-f32d5fc40796',
+    //       snapshotUrls: [
+    //         'https://i.gkd.li/i/14814347', // 收起前
+    //         'https://i.gkd.li/i/14814412', // 收起后
+    //       ],
+    //     },
+    //   ],
+    // },
   ],
 });
